7b768d6d4478e290ff49a502dbdce0b0da9e35a1
siminski <laurensiminski@gmail.com>
2020-08-29 11:58:04 -0400

    fixed index.js file

--- models/index.js
+++ models/index.js
@@ -1,17 +1,17 @@
 "use strict";
 
-const fs = require("fs");
-const path = require("path");
+const fs        = require("fs");
+const path      = require("path");
 const Sequelize = require("sequelize");
-const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
-const db = {};
+const basename  = path.basename(module.filename);
+const env       = process.env.NODE_ENV || "development";
+const config    = require(__dirname + "/../config/config.json")[env];
+const db        = {};
 
 if (config.use_env_variable) {
-  const sequelize = new Sequelize(process.env[config.use_env_variable], {dialect: "mysql"});
+  var sequelize = new Sequelize(process.env[config.use_env_variable], {dialect: "mysql"});
 } else {
-  const sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
